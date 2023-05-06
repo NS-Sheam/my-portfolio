3442823e48751a6078435b77de947f7cf1c676e1
NS-Sheam <123sheamfeni@gmail.com>
2023-05-06 15:56:30 +0600

    mobile menu responsiveness

--- src/pages/shared/Header/Header.jsx
+++ src/pages/shared/Header/Header.jsx
@@ -39,32 +39,36 @@ const Header = () => {
 
             {/* For Mobile  */}
             <section className='my-container-sm py-4 flex justify-between items-start'>
-                <h3 className='text-bandFont text-2xl font-extrabold'>NS-Sheam</h3>
-                <div className='flex flex-col justify-center items-center'>
+                <h3 className='text-bandFont text-xl font-bold'>NS-Sheam</h3>
+                <div className='flex flex-col justify-center items-end'>
                     <div>
-                        <FontAwesomeIcon onClick={() => setIsMenuOpen(false)} className={`text-2xl text-bandFont font-extrabold ${setIsMenuOpen && "hidden"}`} icon={faBars} />
-                        <FontAwesomeIcon onClick={() => setIsMenuOpen(true)} className={`text-2xl text-bandFont font-extrabold ${!setIsMenuOpen && "hidden"}`} icon={faXmark} />
+                        {
+                            isMenuOpen ?
+                            <FontAwesomeIcon onClick={() => setIsMenuOpen(!isMenuOpen)} className={'text-2xl text-bandFont font-extrabold'} icon={faXmark} /> :
+                                <FontAwesomeIcon onClick={() => setIsMenuOpen(!isMenuOpen)} className={'text-2xl text-bandFont font-extrabold'} icon={faBars} /> 
+                                
+                        }
                     </div>
-                    <div className={`sm-menu-item relative -left-80`}>
-                        <ul className='header-list text-bandFont mb-2 text-xl flex flex-col items-center'>
-                            <li className='my-2'>
+                    <div className={`sm-menu-item relative  transform duration-500 ${isMenuOpen ? "right-0" : "-right-80"}`}>
+                        <ul className='header-list text-bandFont mb-2 text-xl flex flex-col items-center gap-2'>
+                            <li className=''>
                                 <ActiveLink to="/">Home</ ActiveLink>
                             </li>
-                            <li className='my-2'>
+                            <li className=''>
                                 <ActiveLink to="/">About</ActiveLink>
                             </li>
-                            <li className='mt-2'>
+                            <li className=''>
                                 <ActiveLink to="/">Contact</ActiveLink>
                             </li>
 
-                        </ul>
-                    </div>
-                </div>
-                <ActiveLink className="my-2">
+                <ActiveLink className="">
                     <button className='text-[#d207d9] border-2 border-[#d207d9] py-2 px-2 hover:border-bandFont hover:text-bandFont text-xl'>
                         Contact me
                     </button>
                 </ActiveLink>
+                        </ul>
+                    </div>
+                </div>
             </section>
         </>
     );
